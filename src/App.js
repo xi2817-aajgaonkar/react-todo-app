b56fb2609d70ac399ca0a5c926037faa91a9242c
xi2817-aajgaonkar <atharva.ajgaonkar@xebia.com>
2021-11-02 13:23:39 +0530

    todo apps components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { SearchBox } from "./Components/SearchBox";
+import { TodoList } from "./Components/TODOList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchBox setTodos={setTodos} />
+        <TodoList todos={todos} />
       </header>
     </div>
   );
